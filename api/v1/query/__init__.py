b8565dda7f23b3efc1094929b07ec6b9b903f3d4
Aleks Rutins <keeper@farthergate.com>
2025-05-27 15:47:40 -0400

    feat: CSV query endpoint

--- api/v1/query/__init__.py
+++ api/v1/query/__init__.py
@@ -1,6 +1,9 @@
+import io
 from typing import Annotated, Any
 from fastapi import APIRouter, Depends, Request, Response
+from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
+import pandas as pd
 
 from api.v1.auth.utils import require_user
 from api.v1.db import user_db
@@ -17,9 +20,7 @@ class QueryResponse(BaseModel):
     columns: list[str]
     values: list[list[Any]]
 
-@router.post('')
-async def query(email: Annotated[str, Depends(require_user)], req: QueryRequest, hreq: Request, resp: Response) -> QueryResponse:
-    resp.headers['Access-Control-Allow-Origin'] = hreq.headers['Origin'] if 'Origin' in hreq.headers else '*'
+async def query_base(email: str, req: QueryRequest) -> QueryResponse: 
     full_access: bool = await db.query_one('select fullAccess from AuthorizedUsers where email=%s', (email,))
     
     async with (db.pool.connection() if full_access else user_db.open(email)) as conn:
@@ -29,3 +30,24 @@ async def query(email: Annotated[str, Depends(require_user)], req: QueryRequest,
             columns=[c.name for c in res.description] if res.description != None else [],
             values=(await res.fetchall()) if res.description != None else []
         )
+
+@router.post('')
+async def query(email: Annotated[str, Depends(require_user)], req: QueryRequest, hreq: Request, resp: Response) -> QueryResponse:
+    resp.headers['Access-Control-Allow-Origin'] = hreq.headers['Origin'] if 'Origin' in hreq.headers else '*'
+    return await query_base(email, req)
+
+@router.post('.csv')
+async def query_csv(email: Annotated[str, Depends(require_user)], req: QueryRequest, hreq: Request) -> QueryResponse:
+    """Query the database, returning the response as a CSV table instead of JSON."""
+    qres = await query_base(email, req)
+
+    df = pd.DataFrame.from_records(qres.values, columns=qres.columns)
+    stream = io.StringIO()
+    df.to_csv(stream, index=False)
+
+    resp = StreamingResponse(iter([stream.getvalue()]), media_type='text/csv')
+
+    resp.headers['Access-Control-Allow-Origin'] = hreq.headers['Origin'] if 'Origin' in hreq.headers else '*'
+    resp.headers['Content-Disposition'] = 'attachment;filename=query.csv'
+
+    return resp
