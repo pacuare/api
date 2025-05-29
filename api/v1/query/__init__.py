a5d1a46865aebc764f58803f9ed579625d591657
Aleks Rutins <keeper@farthergate.com>
2025-05-28 23:16:36 -0400

    feat(query): allow either JSON or form request

--- api/v1/query/__init__.py
+++ api/v1/query/__init__.py
@@ -1,7 +1,8 @@
 import io
-from typing import Annotated, Any
-from fastapi import APIRouter, Depends, Request, Response
+from typing import Annotated, Any, Iterable, Sequence, cast
+from fastapi import APIRouter, Depends, Form, Request, Response
 from fastapi.responses import StreamingResponse
+from psycopg.abc import Query
 from pydantic import BaseModel
 import pandas as pd
 
@@ -14,7 +15,7 @@ router = APIRouter()
 
 class QueryRequest(BaseModel):
     query: str
-    params: list[str] | None = None
+    params: Iterable[str] | None = None
 
 class QueryResponse(BaseModel):
     columns: list[str]
@@ -24,20 +25,25 @@ async def query_base(email: str, req: QueryRequest) -> QueryResponse:
     full_access: bool = await db.query_one('select fullAccess from AuthorizedUsers where email=%s', (email,))
     
     async with (db.pool.connection() if full_access else user_db.open(email)) as conn:
-        res = await conn.execute(req.query, tuple(req.params))
+        res = await conn.execute(cast(Query, req.query), tuple(req.params if req.params is not None else []))
 
         return QueryResponse(
             columns=[c.name for c in res.description] if res.description != None else [],
-            values=(await res.fetchall()) if res.description != None else []
+            values=cast(list[list[Any]], await res.fetchall()) if res.description is not None else []
         )
 
+def extract_query(form_data: Annotated[QueryRequest | None, Form()], json_data: QueryRequest | None) -> QueryRequest:
+    result = form_data if form_data is not None else json_data
+    assert result is not None
+    return result
+
 @router.post('')
-async def query(email: Annotated[str, Depends(require_user)], req: QueryRequest, hreq: Request, resp: Response) -> QueryResponse:
+async def query(email: Annotated[str, Depends(require_user)], req: Annotated[QueryRequest, Depends(extract_query)], hreq: Request, resp: Response) -> QueryResponse:
     resp.headers['Access-Control-Allow-Origin'] = hreq.headers['Origin'] if 'Origin' in hreq.headers else '*'
     return await query_base(email, req)
 
 @router.post('.csv')
-async def query_csv(email: Annotated[str, Depends(require_user)], req: QueryRequest, hreq: Request) -> QueryResponse:
+async def query_csv(email: Annotated[str, Depends(require_user)], req: Annotated[QueryRequest, Depends(extract_query)], hreq: Request) -> StreamingResponse:
     """Query the database, returning the response as a CSV table instead of JSON."""
     qres = await query_base(email, req)
 
