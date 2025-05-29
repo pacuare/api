d5fbabf09b9a793b521916bfad81ebfa7a28debd
Aleks Rutins <keeper@farthergate.com>
2025-05-28 23:41:01 -0400

    Revert "fix(query): parse the body manually"
    This reverts commit a89270bb0ff76bb13f768406bb57f74a975a7a08.
    
    Revert "fix(query): make optional params actually optional"
    
    This reverts commit f17a16f63977ab0dfa9c06a9d3ad5a2cdc724575.
    
    Revert "feat(query): allow either JSON or form request"
    
    This reverts commit a5d1a46865aebc764f58803f9ed579625d591657.

--- api/v1/query/__init__.py
+++ api/v1/query/__init__.py
@@ -1,8 +1,7 @@
 import io
-from typing import Annotated, Any, Iterable, Sequence, cast
-from fastapi import APIRouter, Depends, Form, HTTPException, Request, Response
+from typing import Annotated, Any
+from fastapi import APIRouter, Depends, Request, Response
 from fastapi.responses import StreamingResponse
-from psycopg.abc import Query
 from pydantic import BaseModel
 import pandas as pd
 
@@ -15,7 +14,7 @@ router = APIRouter()
 
 class QueryRequest(BaseModel):
     query: str
-    params: Iterable[str] | None = None
+    params: list[str] | None = None
 
 class QueryResponse(BaseModel):
     columns: list[str]
@@ -25,33 +24,20 @@ async def query_base(email: str, req: QueryRequest) -> QueryResponse:
     full_access: bool = await db.query_one('select fullAccess from AuthorizedUsers where email=%s', (email,))
     
     async with (db.pool.connection() if full_access else user_db.open(email)) as conn:
-        res = await conn.execute(cast(Query, req.query), tuple(req.params if req.params is not None else []))
+        res = await conn.execute(req.query, tuple(req.params))
 
         return QueryResponse(
             columns=[c.name for c in res.description] if res.description != None else [],
-            values=cast(list[list[Any]], await res.fetchall()) if res.description is not None else []
+            values=(await res.fetchall()) if res.description != None else []
         )
 
-# Adapted from https://stackoverflow.com/a/74015930
-async def extract_query(req: Request) -> QueryRequest:
-    content_type = req.headers.get('Content-Type')
-    if content_type is None:
-        content_type = 'application/json'
-    if content_type == 'application/json':
-        return QueryRequest.model_validate(await req.json())
-    elif (content_type == 'application/x-www-form-urlencoded' or
-          content_type.startswith('multipart/form-data')):
-        return QueryRequest.model_validate(await req.form())
-    else:
-        raise HTTPException(400, 'Invalid Content-Type')
-
 @router.post('')
-async def query(email: Annotated[str, Depends(require_user)], req: Annotated[QueryRequest, Depends(extract_query)], hreq: Request, resp: Response) -> QueryResponse:
+async def query(email: Annotated[str, Depends(require_user)], req: QueryRequest, hreq: Request, resp: Response) -> QueryResponse:
     resp.headers['Access-Control-Allow-Origin'] = hreq.headers['Origin'] if 'Origin' in hreq.headers else '*'
     return await query_base(email, req)
 
 @router.post('.csv')
-async def query_csv(email: Annotated[str, Depends(require_user)], req: Annotated[QueryRequest, Depends(extract_query)], hreq: Request) -> StreamingResponse:
+async def query_csv(email: Annotated[str, Depends(require_user)], req: QueryRequest, hreq: Request) -> QueryResponse:
     """Query the database, returning the response as a CSV table instead of JSON."""
     qres = await query_base(email, req)
 
