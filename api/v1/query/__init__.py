5e900eca92c775731e8fc75bf94f8b58cf98f575
Aleks Rutins <keeper@farthergate.com>
2025-05-29 08:14:35 -0400

    fix(query): move response to end of function

--- api/v1/query/__init__.py
+++ api/v1/query/__init__.py
@@ -21,9 +21,13 @@ class QueryResponse(BaseModel):
     columns: list[str]
     values: list[list[Any]]
 
-async def query_base(email: str, req: QueryRequest, hreq: Request, resp: Response) -> QueryResponse: 
+def set_query_headers(hreq: Request, resp: Response):
     resp.headers['access-control-allow-origin'] = hreq.headers['origin'] if 'origin' in hreq.headers else '*'
 
+
+async def query_base(email: str, req: QueryRequest, hreq: Request, resp: Response | None) -> QueryResponse: 
+    if resp is not None:
+        set_query_headers(hreq, resp)    
     full_access: bool = await db.query_one('select fullAccess from AuthorizedUsers where email=%s', (email,))
     
     async with (db.pool.connection() if full_access else user_db.open(email)) as conn:
@@ -45,13 +49,14 @@ async def query_form(email: Annotated[str, Depends(require_user)], req: Annotate
 @router.post('.csv')
 async def query_csv(email: Annotated[str, Depends(require_user)], req: QueryRequest, hreq: Request) -> StreamingResponse:
     """Query the database, returning the response as a CSV table instead of JSON."""
+    
+    qres = await query_base(email, req, hreq, None)
     stream = io.StringIO()
-    resp = StreamingResponse(stream, media_type='text/csv')
-    qres = await query_base(email, req, hreq, resp)
-
+    
     df = pd.DataFrame.from_records(qres.values, columns=qres.columns)
     df.to_csv(stream, index=False)
-
+    resp = StreamingResponse(iter([stream.getvalue()]), media_type='text/csv')
+    set_query_headers(hreq, resp)
     resp.headers['Content-Disposition'] = 'attachment;filename=query.csv'
 
     return resp
