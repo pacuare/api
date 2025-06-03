2980bd0159d15319a38cd4f9a0b59164c95c85c4
Aleks Rutins <keeper@farthergate.com>
2025-06-03 13:27:45 -0400

    Use Scalar for API docs

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
 
 from . import auth, query, db
 
@@ -11,3 +12,15 @@ app = FastAPI(
 app.include_router(auth.router, prefix='/auth')
 app.include_router(query.router, prefix='/query')
 app.include_router(db.router, prefix='/db')
+
+@app.get("/scalar", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(
+        openapi_url='/v1/openapi.json',
+        servers=[
+            {
+                'url': '/v1'
+            }
+        ],
+        title=app.title
+    )
